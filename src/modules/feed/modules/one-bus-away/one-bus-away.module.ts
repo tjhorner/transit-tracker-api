d7b494a09f5ccf1175f8e76ba2ae903533e8cc82
TJ Horner <me@tjhorner.com>
2025-12-20 22:19:54 -0800

    actually inject instrumentation service

--- src/modules/feed/modules/one-bus-away/one-bus-away.module.ts
+++ src/modules/feed/modules/one-bus-away/one-bus-away.module.ts
@@ -1,11 +1,16 @@
 import { Module } from "@nestjs/common"
 import { FeedCacheModule } from "../feed-cache/feed-cache.module"
+import { OneBusAwayInstrumentationService } from "./instrumentation.service"
 import { OneBusAwayService } from "./one-bus-away.service"
 import { oneBusAwaySdkProvider } from "./sdk.provider"
 
 @Module({
   imports: [FeedCacheModule],
-  providers: [oneBusAwaySdkProvider, OneBusAwayService],
+  providers: [
+    OneBusAwayInstrumentationService,
+    oneBusAwaySdkProvider,
+    OneBusAwayService,
+  ],
   exports: [OneBusAwayService],
 })
 export class OneBusAwayModule {}
