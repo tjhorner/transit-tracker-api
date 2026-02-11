a2efa66e42a833c6f62a2f3ed63d98a1033406c7
TJ Horner <me@tjhorner.com>
2026-02-11 10:59:48 -0800

    various cache optimizations: remove compression, use JSON.stringify/parse for serialization

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -1,14 +1,8 @@
-import KeyvBrotli from "@keyv/compress-brotli"
 import KeyvRedis from "@keyv/redis"
 import { Global, Module } from "@nestjs/common"
 import { Cacheable, createKeyv as createKeyvMemory } from "cacheable"
 import Keyv from "keyv"
 import ms from "ms"
-import zlib from "node:zlib"
-
-const {
-  constants: { BROTLI_PARAM_QUALITY },
-} = zlib
 
 @Global()
 @Module({
@@ -23,13 +17,8 @@ const {
           }),
           secondary: process.env.REDIS_URL
             ? new Keyv({
-                compression: new KeyvBrotli({
-                  compressOptions: {
-                    params: {
-                      [BROTLI_PARAM_QUALITY]: 1,
-                    },
-                  },
-                }),
+                serialize: JSON.stringify,
+                deserialize: JSON.parse,
                 store: new KeyvRedis(process.env.REDIS_URL, {
                   namespace: "cache",
                 }),
