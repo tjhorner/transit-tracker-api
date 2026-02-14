ce031db7c8fa8e8db606520de8085a1b6314fa39
TJ Horner <me@tjhorner.com>
2026-02-13 16:23:44 -0800

    cache gtfs metrics

--- src/modules/feed/modules/gtfs/gtfs-metrics.service.ts
+++ src/modules/feed/modules/gtfs/gtfs-metrics.service.ts
@@ -2,7 +2,8 @@ import { forwardRef, Inject, Injectable, Logger, Scope } from "@nestjs/common"
 import { MetricService } from "nestjs-otel"
 import { Pool } from "pg"
 import { PG_POOL } from "./gtfs.module"
-import { getFeedSizes } from "./queries/get-feed-sizes.queries"
+import { getFeedSizes, IGetFeedSizesResult } from "./queries/get-feed-sizes.queries"
+import { Cacheable } from "cacheable"
 
 @Injectable({ scope: Scope.DEFAULT })
 export class GtfsMetricsService {
@@ -12,7 +13,25 @@ export class GtfsMetricsService {
   constructor(
     @Inject(forwardRef(() => PG_POOL)) private readonly pool: Pool,
     private readonly metricService: MetricService,
+    private readonly cacheManager: Cacheable,
   ) {}
+
+  private async getFeedSizes(): Promise<IGetFeedSizesResult[]> {
+    const cacheKey = "gtfsFeedSizes"
+    const cached = await this.cacheManager.get<IGetFeedSizesResult[]>(cacheKey)
+    if (cached) {
+      return cached
+    }
+
+    const conn = await this.pool.connect()
+    try {
+      const res = await getFeedSizes.run(undefined, conn)
+      await this.cacheManager.set(cacheKey, res, "5m")
+      return res
+    } finally {
+      conn.release()
+    }
+  }
 
   activate() {
     if (this.active) {
@@ -28,18 +47,12 @@ export class GtfsMetricsService {
         unit: "megabytes",
       })
       .addCallback(async (observable) => {
-        const conn = await this.pool.connect()
-
-        try {
-          const res = await getFeedSizes.run(undefined, conn)
-          for (const row of res) {
-            observable.observe(row.size_mb, {
-              feed_code: row.feed_code,
-              table: row.table_name,
-            })
-          }
-        } finally {
-          conn.release()
+        const sizes = await this.getFeedSizes()
+        for (const row of sizes) {
+          observable.observe(row.size_mb, {
+            feed_code: row.feed_code,
+            table: row.table_name,
+          })
         }
       })
   }
