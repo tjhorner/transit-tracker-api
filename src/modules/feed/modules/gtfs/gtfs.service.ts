297aa1be020bd063e32e6e964ef38a2717788682
saucylegs <37064488+saucylegs@users.noreply.github.com>
2026-02-21 04:29:54 -0800

    add vehicles to e2e tests

--- src/modules/feed/modules/gtfs/gtfs.service.ts
+++ src/modules/feed/modules/gtfs/gtfs.service.ts
@@ -400,7 +400,7 @@ export class GtfsService implements FeedProvider {
           stopName: staticTrip.stop_name ?? "Unnamed Stop",
           arrivalTime,
           departureTime,
-          vehicle,
+          vehicle: isRealtime ? vehicle : null,
           isRealtime,
         })
       })
