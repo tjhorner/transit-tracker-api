9534b0086eadaf8d2de854664e01d5721c798862
TJ Horner <me@tjhorner.com>
2026-04-07 19:33:18 -0700

    fix: properly exit after CLI finishes
    kinda hacky tho

--- src/cli.ts
+++ src/cli.ts
@@ -2,13 +2,19 @@ import { CommandFactory } from "nest-commander"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  await CommandFactory.run(AppModule, [
+  const app = await CommandFactory.createWithoutRunning(AppModule, [
     "verbose",
     "log",
     "debug",
     "error",
     "warn",
   ])
+
+  app.enableShutdownHooks()
+  await CommandFactory.runApplication(app)
+  await app.close()
+
+  process.exit(0)
 }
 
 bootstrap()
