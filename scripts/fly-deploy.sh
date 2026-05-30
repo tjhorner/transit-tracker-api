60b2526e11d1ad7d40590b460b0e0396068f1154
TJ Horner <me@tjhorner.com>
2026-05-29 18:50:11 -0700

    chore: tweak autoscaling config

--- scripts/fly-deploy.sh
+++ scripts/fly-deploy.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+echo "Stopping autoscaler..."
+
+AUTOSCALER_MACHINES=$(fly machine list -a transit-tracker-api-autoscaler --json | jq -r '.[].id')
+fly machine stop -a transit-tracker-api-autoscaler $AUTOSCALER_MACHINES
+
 fly deploy $@
 
 FLY_MACHINES=$(fly machine list --json)
@@ -9,3 +14,6 @@ SYNC_MACHINE_ID=$(echo $FLY_MACHINES | jq -r '.[] | select(.config.metadata.sche
 LATEST_IMAGE=$(echo $FLY_MACHINES | jq -r '[.[] | select(.config.metadata.fly_process_group == "app")] | sort_by(.updated_at)[0] | .image_ref | .registry + "/" + .repository + ":" + .tag')
 
 fly machine update -y -i $LATEST_IMAGE $SYNC_MACHINE_ID
+
+echo "Resuming autoscaler..."
+fly machine start -a transit-tracker-api-autoscaler $AUTOSCALER_MACHINES
