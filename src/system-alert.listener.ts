a96cb4b4e137140b258838611311fab195e63142
TJ Horner <me@tjhorner.com>
2026-06-09 18:05:27 -0700

    chore: emoji 👁️👁️

--- src/system-alert.listener.ts
+++ src/system-alert.listener.ts
@@ -13,21 +13,21 @@ export class SystemAlertListener {
 
   @OnEvent(FeedSyncService.ErrorEvent, { async: true })
   async handleFeedSyncError(event: FeedSyncErrorEvent) {
-    const title = "Feed Sync Error"
-    const message = `A fatal error occurred during feed sync (Sentry ID: ${event.sentryId}):\n\n${event.message}`
+    const title = "💥 Feed Sync Error"
+    const message = `A fatal error occurred during feed sync (Sentry ID: ${event.sentryId}):\n\n\n${event.message}`
     await this.notificationsService.sendNotification(title, message)
   }
 
   @OnEvent(FeedSyncService.WarnEvent, { async: true })
   async handleFeedSyncWarn(event: FeedSyncWarnEvent) {
-    const title = `Feed Sync Warning - ${event.feedCode}`
-    const message = `A warning occurred during feed sync for feed "${event.feedCode}" (Sentry ID: ${event.sentryId}):\n\n${event.message}`
+    const title = `⚠️ Feed Sync Warning - ${event.feedCode}`
+    const message = `A warning occurred during feed sync for feed "${event.feedCode}" (Sentry ID: ${event.sentryId}):\n\n\n${event.message}`
     await this.notificationsService.sendNotification(title, message)
   }
 
   @OnEvent(FeedSyncService.CompletedEvent, { async: true })
   async handleFeedSyncCompleted() {
-    const title = "Feed Sync Completed"
+    const title = "✅ Feed Sync Completed"
     const message = "Feed synchronization has completed successfully."
     await this.notificationsService.sendNotification(title, message)
   }
